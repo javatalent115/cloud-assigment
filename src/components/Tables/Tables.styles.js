fd7177ca31556bb39542fd72b79798aa816064b0
Việt Trần Vũ Hoàng <zieeettt@Viets-MacBook-Air.local>
2021-08-30 17:57:26 +0700

    update

--- src/components/Tables/Tables.styles.js
+++ src/components/Tables/Tables.styles.js
@@ -4,6 +4,11 @@ export const Wrapper = styled.div`
     margin: 40px 0px;
 `;
 export const Content = styled.div`
+    div {
+        height: 500px;
+        overflow: auto;
+    }
+    height: 600px;
     width: 1368px;
     border: 1px solid rgba(38, 56, 150, 0.1411764705882353);
     box-shadow: 0 4px 12px 0 rgb(34 41 47 / 12%);
@@ -11,10 +16,23 @@ export const Content = styled.div`
     border: 1px solid rgba(38, 56, 150, 0.1411764705882353);
     border-radius: 10px;
     padding: 24px;
-    display: block;
     background-color: #fff;
     h5 {
         font-weight: bold;
         margin-bottom: 20px;
+    }
+    tbody,
+    thead {
+        text-align: center;
+    }
+    tbody td {
+    }
+
+    thead th {
+        background: white;
+        position: sticky;
+        top: 0;
+        border-top: 2px solid #dee2e6;
+        border-bottom: 2px solid #dee2e6 !important;
     }
 `;
--- src/components/Tables/index.js
+++ src/components/Tables/index.js
@@ -3,6 +3,8 @@ import { Content, Wrapper } from "./Tables.styles";
 import { Table } from "react-bootstrap";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { numberFormat } from "../../numberFormat";
+import "./tableCustom.css";
 const Tables = () => {
     const [information, setInformation] = useState({});
     async function fetchData() {
@@ -20,34 +22,59 @@ const Tables = () => {
         <Wrapper>
             <Content>
                 <h5>World covid information</h5>
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                            <th>Country Name</th>
-                            <th>Confirmed</th>
-                            <th>Deaths</th>
-                            <th>Population</th>
-                            <th>Life expectancy</th>
-                            <th>Continent</th>
-                            <th>Abbreviation</th>
-                            <th>Location</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {Object.keys(information).map((key, index) => (
-                            <tr key={information[Object.keys(key)[index]]}>
-                                <td> {Object.keys(information)[index]}</td>
-                                <td> {Object.values(information)[index].All.confirmed}</td>
-                                <td> {Object.values(information)[index].All.deaths}</td>
-                                <td> {Object.values(information)[index].All.population}</td>
-                                <td> {Object.values(information)[index].All.life_expectancy}</td>
-                                <td> {Object.values(information)[index].All.continent}</td>
-                                <td> {Object.values(information)[index].All.abbreviation}</td>
-                                <td> {Object.values(information)[index].All.location}</td>
+                <div>
+                    <Table
+                        striped
+                        bordered
+                        hover
+                        style={{ borderCollapse: "none", borderSpacing: "0px" }}
+                    >
+                        <thead>
+                            <tr>
+                                <th>Country Name</th>
+                                <th>Confirmed</th>
+                                <th>Deaths</th>
+                                <th>Population</th>
+                                <th>Life expectancy</th>
+                                <th>Continent</th>
+                                <th>Abbreviation</th>
+                                <th>Location</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </Table>
+                        </thead>
+                        <tbody>
+                            {Object.keys(information).map((key, index) => (
+                                <tr key={Object.keys(key)[index]}>
+                                    <td> {Object.keys(information)[index]}</td>
+
+                                    <td>
+                                        {numberFormat(
+                                            Object.values(information)[index].All.confirmed
+                                        )}
+                                    </td>
+                                    <td>
+                                        {" "}
+                                        {numberFormat(Object.values(information)[index].All.deaths)}
+                                    </td>
+                                    <td>
+                                        {" "}
+                                        {numberFormat(
+                                            Object.values(information)[index].All.population
+                                        )}
+                                    </td>
+                                    <td>
+                                        {" "}
+                                        {numberFormat(
+                                            Object.values(information)[index].All.life_expectancy
+                                        )}
+                                    </td>
+                                    <td> {Object.values(information)[index].All.continent}</td>
+                                    <td> {Object.values(information)[index].All.abbreviation}</td>
+                                    <td> {Object.values(information)[index].All.location}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+                </div>
             </Content>
         </Wrapper>
     );
